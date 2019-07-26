a923c444496311c0bd89bfb17f278ca24aaf8e6b
joachim.hagege <joachim.hagege@alibaba-inc.com>
2019-07-26 16:38:18 +0300

    test_my_lists: precreate sessions, move login checks into base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,7 +7,6 @@ import time
 MAX_WAIT = 10
 
 
-
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -16,10 +15,8 @@ class FunctionalTest(StaticLiveServerTestCase):
         if staging_server:
             self.live_server_url = 'http://' + staging_server
 
-
     def tearDown(self):
         self.browser.quit()
-
 
     def wait_for(self, fn):
         start_time = time.time()
@@ -30,8 +27,6 @@ class FunctionalTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
-
-
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -46,7 +41,19 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
 
+    def wait_to_be_logged_in(self, email):
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Log out')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
